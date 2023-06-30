0450502156c4318dfa77b6b7478290b274431990
jithus13 <128806027+jithus13@users.noreply.github.com>
2023-07-01 00:15:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-// import React from 'react';
-// import UserSearch from './usersearch';
-
-
-// const App = () => {
-//   return (
-//     <div className="app">
-//       <h1>User List App</h1>
-//       <UserSearch />
-//     </div>
-//   );
-// };
-
-
-// export default App;
 import React from 'react';
+import UserSearch from './usersearch';
 
-function App() {
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello, React!</h1>
+    <div className="app">
+      <h1>User List App</h1>
+      <UserSearch />
     </div>
   );
-}
+};
+
 
 export default App;
+
